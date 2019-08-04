8e7d11fc293901ad82dbc793b74613ccb26dbd0b
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-08-03 22:26:51 -0400

    article and comment js schema created

---
+++ models/Article.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    require: true
+  },
+  comment: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
