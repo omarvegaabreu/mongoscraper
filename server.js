ada9376ae9e899ced7922466d849c26d07f51193
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-08-03 21:35:22 -0400

    mongo connection and servers are running

--- server.js
+++ server.js
@@ -18,11 +18,23 @@ app.use(
   })
 );
 
+//route to public folder
+app.use(express.static(process.cwd() + "/public"));
+
 //handlebars middleware
 
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+//mongoose
+mongoose.connect("mongodb://localhost/scrapped_news");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to Mongoose!");
+});
 
 //PORT
 const PORT = process.env.PORT || 3000;
