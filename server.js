d1c6de018231e8df73ac1955721cf47b3d110941
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-08-04 21:12:31 -0400

    updated server

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const http = require("http");
-
 //initialize Express app
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: true
   })
 );
 
 app.use(express.static(process.cwd() + "/public"));
 //Require set up handlebars
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 app.engine(
   "handlebars",
   exphbs({
@@ -28,7 +28,7 @@ app.set("view engine", "handlebars");
 
 //connecting to MongoDB
 //mongoose.connect("mongodb://localhost/scraped_news");
-const db = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper";
+let db = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper";
 // mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 mongoose
@@ -40,7 +40,7 @@ var routes = require("./controllers/controllers");
 app.use("/", routes);
 
 //Create localhost port.
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on PORT " + port);
+
+app.listen(PORT, function() {
+  console.log("Listening on PORT " + PORT);
 });
